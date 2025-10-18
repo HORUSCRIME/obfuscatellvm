b0d4c60fc2ae7caf1e77389c010527015d2a29f0
Momentum-666 <k.s.sushanth06@gmail.com>
2025-10-18 07:43:56 +0530

    first commit

---
+++ tooling/mingw-toolchain.cmake
@@ -0,0 +1,28 @@
+# CMake toolchain file for MinGW cross-compilation
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# Specify the cross compiler
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+# Where to find the target environment
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+
+# Search for programs in the build host directories
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# Search for libraries and headers in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Set Windows-specific flags
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
+
+# LLVM configuration for MinGW
+set(LLVM_HOST_TRIPLE "x86_64-w64-mingw32")
+set(LLVM_DEFAULT_TARGET_TRIPLE "x86_64-w64-mingw32")
